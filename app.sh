43de65d6cdf4439262bb16255d5340e4de6b413b
Francois Stephany <tulipe.moutarde@gmail.com>
2014-07-15 17:43:42 +0200

    Add some comments about the desired behavior of app.sh

---
+++ app.sh
@@ -0,0 +1,14 @@
+
+# Usage:
+
+# app install
+# -> Run the install.st script on the image then save and quit
+
+# app start
+# -> Run the start.st script on the image. Store the process PID
+
+# app stop
+# -> Check the PID stored when started and kill the process associated with it.
+
+# app restart
+# -> stop and start the app.
