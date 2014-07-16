30fc13f80f8b170c58604ee8a0ae0fef41feb00d
Francois Stephany <tulipe.moutarde@gmail.com>
2014-07-16 14:58:12 +0200

    Ansible scripts in progress

--- app.sh
+++ app.sh
@@ -1,3 +1,39 @@
+#!/bin/bash
+# This script is inspired by st-exec.sh from http://stfx.eu/pharo-server/
+# originally written by Sven Van Caekenberghe
+
+function usage() {
+    cat <<END
+Usage: $0 <command> <image>
+    manage a Smalltalk server
+Naming
+    script.pid  will be used to hold the process id
+    image.image is the Smalltalk image that will be started
+Commands:
+    install  run the install script on the image.
+    start    start the server in background
+    stop     stop the server
+    restart  restart the server
+    run      run the server in foreground
+    pid      print the process id
+END
+    exit 1
+}
+
+# Setup vars
+
+script_home=$(dirname $0)
+script_home=$(cd $script_home && pwd)
+
+command=$2
+image=$3
+
+echo Working directory $script_home
+
+# We need the two arguments.
+if [ "$#" -ne 2 ]; then
+    usage
+fi
 
 # Usage:
 
